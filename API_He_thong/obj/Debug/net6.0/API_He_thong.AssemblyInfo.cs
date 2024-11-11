6762029c8645cf1e8ff96ed5a73d9b0cd42bc036
trananhtungdotnet <tungtapd09419@fpt.edu.vn>
2024-11-11 09:47:02 +0700

    update logo

--- API_He_thong/obj/Debug/net6.0/API_He_thong.AssemblyInfo.cs
+++ API_He_thong/obj/Debug/net6.0/API_He_thong.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_He_thong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f6444715a456b03ce98ea1d2aa12a85c49f845")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec321ca4afc55289449e5dbb274ebef853ea5dee")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_He_thong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_He_thong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
